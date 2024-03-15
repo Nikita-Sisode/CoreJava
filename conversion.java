e4e6783b5609f613c641ce724efeff143c7ac8db
Nikita-Sisode <161593597+Nikita-Sisode@users.noreply.github.com>
2024-03-15 22:30:08 +0530

    Add files via upload

--- conversion.java
+++ conversion.java
@@ -1,7 +1,7 @@
 /*Write a program that demonstrates widening conversion from int to double and prints the result */
 public class conversion {
     public static void main(String[] args) {
-        int i = 86;    //covert integer value into double
+        int i = 86;    //convert integer value into double
         double d = i; // Using assignment operator to convert int to double
 
         System.out.println("Original int value: " + i);
