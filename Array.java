a7798a79d7a68fd0a45077260b5efb95f2acdced
Nikita-Sisode <161593597+Nikita-Sisode@users.noreply.github.com>
2024-03-20 12:36:02 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,16 @@
+
+/*Write a program to print elements of Array ?  */
+
+class Array{
+ 
+    public static void main(String[] args) {
+        System.out.println("Elements of Array:");
+        int a[]={2,3,4,5,6,7};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+        System.out.println(a[5]);
+    }
+}
